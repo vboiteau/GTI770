6ac0dde85aea2580dc29174adf5fa37250ccc374
Laurent O'Donoughue <l.odonoughue@gmail.com>
2017-07-12 14:40:37 -0400

    base

---
+++ lab04/src/Single.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Olivier on 2017-06-02.
+ */
+
+import weka.classifiers.meta.FilteredClassifier;
+import weka.classifiers.trees.J48;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
+
+import java.io.PrintWriter;
+
+public class Single {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
